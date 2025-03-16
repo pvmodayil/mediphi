c85aeb23f3092d17eb36e8087a7dafc7be6b1112
Philip Modayil <philipv.modayil@gmail.com>
2025-03-16 12:47:05 +0100

    Commit for the day

--- mediphi/mediphi.py
+++ mediphi/mediphi.py
@@ -7,9 +7,19 @@
 #####################################################################################
 #                                     Imports
 #####################################################################################
+import uuid
 import pdfplumber
+import pandas as pd
+from ._types import ExtractedItems
 
 def processPDF(pdf_path: str) -> None:
+    extracted_items: list[ExtractedItems] = []
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
-            pass
+            page_content_text: str = page.extract_text()
+            page_content_tables:list[list[list]] = page.extract_tables()
+            
+            # Will only go through loop if tables exist
+            for table_id,table in enumerate(page_content_tables):
+                # Convert extracted table to pandas dataframe
+                df: pd.DataFrame = pd.DataFrame(table[1:], columns=table[0])  # Create DataFrame
