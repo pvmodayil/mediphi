b43adb7592606a845541cd71f05a7bccf85114fb
Philip Modayil <philipv.modayil@gmail.com>
2025-06-04 13:47:46 +0200

    Added qr code to profile section

---
+++ app/profile/layout.tsx
@@ -0,0 +1,13 @@
+export default function AuthLayout({ children }: {
+  readonly children: React.ReactNode;
+}) {
+  return (
+    <main>
+    <div className = "min-h-screen bg-gradient-to-br from-green-500 via-emerald-500 to-cyan-500 flex items-center justify-center p-5">
+      <div className="bg-amber-50 backdrop-blur-lg rounded-3xl shadow-2xl p-20 w-full max-w-md text-center border border-white/20 animate-fade-in-up">
+       {children}
+      </div>
+    </div>
+    </main>
+  );
+}
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,13 +1,33 @@
-import Image from "next/image";
+'use client';
+import React from 'react';
 import Link from "next/link";
+import {QRCodeSVG} from "qrcode.react"
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  dob: string;} 
 
 export default function Profile() {
+
+  const user: User = {
+    id: "12345",  
+    name: "John Doe",
+    email: "example@mail.com",
+    dob: "1990-01-01"
+  };
+  const qrValue = JSON.stringify({user});    
+  
   return (
-    <main>
-      <h1>Profile Page</h1>
-      <h1 className="text-2xl font-bold mb-4">Profile</h1>
-      <p>Welcome to your profile!</p>
-      <Link href="./signout">Log out</Link>
-    </main>
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-4 text-emerald-700">User Details</h1>
+      <div className="mb-4">
+        <h2 className="text-xl font-bold text-emerald-700">Scan Me</h2>
+      </div>
+      <div className="mb-4">
+        <QRCodeSVG value={qrValue} size={256} />
+      </div>
+    </div>
   );
 }
